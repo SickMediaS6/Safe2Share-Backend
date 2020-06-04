0bc448bd95d76bed80e159b05deb3101a59fc9ad
Robin van den Berg <rob-vandenberg@outlook.com>
2020-06-04 10:45:40 +0200

    Added Azure cosmoDB link to mongoose service

--- modules/common/services/mongoose.service.js
+++ modules/common/services/mongoose.service.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const env = require('../config/env.config')
 
 const connectWithRetry = () => {
     console.log('MongoDB connection with retry')
-    mongoose.connect("mongodb://localhost:27017/safe2share", {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    mongoose.connect("mongodb://"+env.azure.COSMOSDB_HOST+":"+env.azure.COSMOSDB_PORT+"/"+env.azure.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb", {
+    auth: {
+        user: env.azure.COSMODDB_USER,
+        password: env.azure.COSMOSDB_PASSWORD
+    }
+    }).then(()=>{
         console.log('MongoDB is connected')
     }).catch(err => {
         console.log('MongoDB connection unsuccessful, retry after 5 seconds. ');
